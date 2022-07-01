1ebd83c19c00fe9399acc151664487a41b7d818c
Bishnu <bishnuthapako@gmail.com>
2022-07-01 16:27:39 -0400

    changes

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-function QuestionList() {
+function QuestionList({question}) {
+  // console.log(question, "result")
+
+ const viewQuestion = question.map((page, index)=>(<li key={index}>{page.prompt}</li>))
+//  console.log(viewQuestion , "res")
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+     {viewQuestion}
+      </ul>
     </section>
   );
 }
